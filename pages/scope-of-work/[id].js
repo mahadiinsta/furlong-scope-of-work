682e6ae1b19c76fb6662d93116df0c0aa0a043bc
MD MAHADI HASAN <mahadi@instawebworks.com.au>
2022-08-30 11:37:42 +0600

    Updated the data

--- pages/scope-of-work/[id].js
+++ pages/scope-of-work/[id].js
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useEffect, useState } from "react";
 
 import {
   Button,
@@ -11,31 +11,35 @@ import {
   TableHead,
   TableRow,
   Typography,
-} from '@mui/material'
-import { Box } from '@mui/system'
-import axios from 'axios'
-import { convertFromRaw, convertToRaw, EditorState } from 'draft-js'
-import { useRouter } from 'next/dist/client/router'
-import dynamic from 'next/dynamic'
-import Image from 'next/image'
-import 'react-draft-wysiwyg/dist/react-draft-wysiwyg.css'
-import logo from '../../public/Furlong_Logo.jpg'
+} from "@mui/material";
+import { Box } from "@mui/system";
+import axios from "axios";
+import { convertFromRaw, convertToRaw, EditorState } from "draft-js";
+import { useRouter } from "next/dist/client/router";
+import dynamic from "next/dynamic";
+import Image from "next/image";
+import "react-draft-wysiwyg/dist/react-draft-wysiwyg.css";
+import logo from "../../public/Furlong_Logo.jpg";
 const Editor = dynamic(
-  () => import('react-draft-wysiwyg').then((mod) => mod.Editor),
-  { ssr: false },
-)
+  () => import("react-draft-wysiwyg").then((mod) => mod.Editor),
+  { ssr: false }
+);
 
-const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
-  console.log('Test ', { initialDataFromDB }, { project_variations })
-  const router = useRouter()
-  const projectID = router.query.id
+const ScopeOfWork = ({
+  project_variations,
+  initialDataFromDB,
+  project_data,
+}) => {
+  console.log("Test ", { initialDataFromDB }, { project_variations });
+  const router = useRouter();
+  const projectID = router.query.id;
 
   let initialData = {
     blocks: [
       {
-        key: '6c8fc',
-        text: 'Please, Enter Your Text Here....',
-        type: 'unstyled',
+        key: "6c8fc",
+        text: "Please, Enter Your Text Here....",
+        type: "unstyled",
         depth: 0,
         inlineStyleRanges: [],
         entityRanges: [],
@@ -43,72 +47,72 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
       },
     ],
     entityMap: {},
-  }
+  };
 
   if (initialDataFromDB.length > 0) {
-    initialData = JSON.parse(initialDataFromDB[0].content)
+    initialData = JSON.parse(initialDataFromDB[0].content);
   }
   const [editorState, setEditorState] = useState(() =>
-    EditorState.createEmpty(),
-  )
+    EditorState.createEmpty()
+  );
 
-  const [loading, setLoading] = useState(false)
+  const [loading, setLoading] = useState(false);
 
   const publishPost = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const data = {
         projectID: projectID,
         content: JSON.stringify(convertedContent),
-      }
+      };
 
-      const response = await axios.post('/api/scopeofwork/scope-of-work', data)
+      const response = await axios.post("/api/scopeofwork/scope-of-work", data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    router.reload(window.location.pathname)
-  }
+    router.reload(window.location.pathname);
+  };
 
   const updatePost = async () => {
-    setLoading(true)
+    setLoading(true);
     try {
       const data = {
         projectID: projectID,
         content: JSON.stringify(convertedContent),
-      }
+      };
 
-      const response = await axios.put('/api/scopeofwork/scope-of-work', data)
+      const response = await axios.put("/api/scopeofwork/scope-of-work", data);
     } catch (error) {
-      console.log(error)
+      console.log(error);
     }
-    router.reload(window.location.pathname)
-  }
-  const [convertedContent, setConvertedContent] = useState(null)
+    router.reload(window.location.pathname);
+  };
+  const [convertedContent, setConvertedContent] = useState(null);
 
-  const [state, setState] = useState([])
+  const [state, setState] = useState([]);
 
   const onEditorStateChange = (editorState) => {
-    setEditorState(editorState)
-    setConvertedContent(convertToRaw(editorState.getCurrentContent()))
-  }
+    setEditorState(editorState);
+    setConvertedContent(convertToRaw(editorState.getCurrentContent()));
+  };
 
   useEffect(() => {
-    setEditorState(EditorState.createWithContent(convertFromRaw(initialData)))
-  }, [])
+    setEditorState(EditorState.createWithContent(convertFromRaw(initialData)));
+  }, []);
 
   return (
     <>
       {loading === false ? (
-        <Box sx={{ width: '80%', margin: '0 auto' }}>
-          <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+        <Box sx={{ width: "80%", margin: "0 auto" }}>
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
             <Image src={logo} alt="bg" width={200} height={200} />
           </Box>
           <Typography
-            variant="h4"
+            variant="h6"
             align="center"
-            sx={{ fontWeight: 'bold', m: '20px 0px' }}
+            sx={{ fontWeight: "bold", m: "20px 0px" }}
           >
-            Scope of Work
+            {project_data[0].Name || "Scope of Work"}
           </Typography>
           <Box
             sx={{
@@ -116,7 +120,7 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
               mb: 1,
             }}
           >
-            <Box sx={{ border: '1px solid lightgrey', padding: 1 }}>
+            <Box sx={{ border: "1px solid lightgrey", padding: 1 }}>
               <Editor
                 editorState={editorState}
                 onEditorStateChange={onEditorStateChange}
@@ -136,31 +140,31 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
               </Button>
             ) : initialDataFromDB.length > 0 && convertedContent !== null ? (
               <Box
-                sx={{ display: 'flex', justifyContent: 'center', mt: 5, mb: 1 }}
+                sx={{ display: "flex", justifyContent: "center", mt: 5, mb: 1 }}
               >
                 <Button
                   onClick={updatePost}
                   variant="contained"
-                  sx={{ minWidth: 300, margin: '0 auto' }}
+                  sx={{ minWidth: 300, margin: "0 auto" }}
                 >
                   Update
                 </Button>
               </Box>
             ) : (
-              ''
+              ""
             )}
           </Box>
           {project_variations?.length ? (
             <TableContainer component={Paper} sx={{ mb: 2 }}>
               <Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead sx={{ bgcolor: '#000', color: '#fff' }}>
+                <TableHead sx={{ bgcolor: "#000", color: "#fff" }}>
                   <TableRow>
-                    <TableCell sx={{ fontWeight: 'bold', color: '#fff' }}>
+                    <TableCell sx={{ fontWeight: "bold", color: "#fff" }}>
                       Variation Name
                     </TableCell>
                     <TableCell
                       align="left"
-                      sx={{ fontWeight: 'bold', color: '#fff' }}
+                      sx={{ fontWeight: "bold", color: "#fff" }}
                     >
                       Variation Summary
                     </TableCell>
@@ -171,7 +175,7 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
                     <TableRow
                       key={index}
                       sx={{
-                        '&:last-child td, &:last-child th': { border: 0 },
+                        "&:last-child td, &:last-child th": { border: 0 },
                       }}
                     >
                       <TableCell component="th" scope="row">
@@ -186,9 +190,9 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
           ) : (
             <Box
               sx={{
-                display: 'flex',
-                justifyContent: 'center',
-                alignItems: 'center',
+                display: "flex",
+                justifyContent: "center",
+                alignItems: "center",
               }}
             >
               <Typography variant="h3">No Project Variations</Typography>
@@ -196,19 +200,28 @@ const ScopeOfWork = ({ project_variations, initialDataFromDB }) => {
           )}
         </Box>
       ) : (
-        <Box sx={{ display: 'flex', justifyContent: 'center', mt: '20%' }}>
+        <Box sx={{ display: "flex", justifyContent: "center", mt: "20%" }}>
           <CircularProgress size={40} />
         </Box>
       )}
     </>
-  )
-}
+  );
+};
 
-export default ScopeOfWork
+export default ScopeOfWork;
 
 export async function getServerSideProps(context) {
-  const id = context.params.id
-  const accessToken = await axios.get(process.env.ACCESSTOKEN_URL)
+  const id = context.params.id;
+  const accessToken = await axios.get(process.env.ACCESSTOKEN_URL);
+
+  const project_resp = await axios.get(
+    `https://www.zohoapis.com/crm/v2/FP_Projects/${id}`,
+    {
+      headers: {
+        Authorization: accessToken.data.access_token,
+      },
+    }
+  );
 
   const project_variations_resp = await axios.get(
     `https://www.zohoapis.com/crm/v2/FP_Projects/${id}/Project_Variation`,
@@ -216,17 +229,18 @@ export async function getServerSideProps(context) {
       headers: {
         Authorization: accessToken.data.access_token,
       },
-    },
-  )
+    }
+  );
   // console.log({ resp: project_variations_resp?.data?.data || ['NoData'] })
 
   const getInitialDataResp = await axios.get(
-    `https://furlong-scope-of-work.vercel.app//api/scopeofwork/scope-of-work?id=${id}`,
-  )
+    `https://furlong-scope-of-work.vercel.app/api/scopeofwork/scope-of-work?id=${id}`
+  );
   return {
     props: {
       project_variations: project_variations_resp?.data?.data || [],
       initialDataFromDB: getInitialDataResp?.data?.message || [],
+      project_data: project_resp?.data?.data || [],
     },
-  }
+  };
 }
