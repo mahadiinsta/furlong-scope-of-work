9f4575c8905d2df8259720b5b90dcb8aabebb115
MD MAHADI HASAN <mahadi@instawebworks.com.au>
2022-09-14 09:40:59 +0600

    slight changes to the app

--- pages/scope-of-work/[id].js
+++ pages/scope-of-work/[id].js
@@ -127,21 +127,6 @@ const ScopeOfWork = ({
                 wrapperClassName="wrapper-class"
                 editorClassName="editor-class"
                 toolbarClassName="toolbar-class"
-                toolbar={{
-                  options: ['inline', 'blockType', 'fontSize', 'fontFamily', 'list', 'textAlign', 'colorPicker', 'link', 'embedded', 'emoji', 'image', 'remove', 'history'],
-                  fontFamily: {
-                    options: ['Arial'],
-                    className: undefined,
-                    component: undefined,
-                    dropdownClassName: undefined,
-                  },
-                  fontSize: {
-                    options: [12, 14, 16, 18, 24, 30, 36, 48, 60, 72, 96],
-                    className: undefined,
-                    component: undefined,
-                    dropdownClassName: undefined,
-                  },
-                }}
               />
             </Box>
 
